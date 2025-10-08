adcf41e2e2e0084fa56015bb3f09134d37455f25
Tadinada Amaresh <amaresh.tadinada@outlook.com>
2025-10-08 19:21:11 +0530

    Create types.ts

---
+++ samples/types.ts
@@ -0,0 +1,12 @@
+let flag: boolean = true;
+console.log("boolean flag having value:" + flag);
+let decNum: number = 6;
+console.log("number decNum having value:" + decNum);
+let binNum: number = 0b101;
+console.log("number binNum having value:" + binNum);
+let octNum: number = 0o77;
+console.log("number octNum having value:" + octNum);
+let floatNum: number = 1.23;
+console.log("number floatNum having value:" + floatNum);
+let hexNum: number = 0xff;
+console.log("number hex having value:" + hexNum);
