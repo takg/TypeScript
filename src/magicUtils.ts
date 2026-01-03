3ba95ff311490053aceaf469c78b68f8dd4b34d6
Tadinada Amaresh <amaresh.tadinada@outlook.com>
2026-01-03 19:23:20 +0530

    Clean up magicUtils.ts by removing comment
    Removed unnecessary comment from magicUtils.ts

---
+++ src/magicUtils.ts
@@ -0,0 +1,9 @@
+export function castSpell(spell: string): string {
+    return `Casting ${spell} spell! 🔮`;
+}
+
+export function brewPotion(potion: string): string {
+    return `Brewing ${potion} potion! 🧪`;
+}
+
+export const schoolName = "Hogwarts School of Witchcraft and Wizardry";
